f0b6da8a387045b571f1a65099005d58eba6d5c6
Knock <enoch10evelyn@gmail.com>
2022-06-30 19:59:28 -0700

    Repo test

---
+++ friendly-parakeet/Develop/script.js
@@ -0,0 +1,26 @@
+// List Variables
+var length; // user password length select
+var lower; // user lowercase select
+var upper;// uppercase select
+var numeric;// number select
+var special;// special character select
+var selected; // Choice that the user selects
+
+//Declaring arrays
+
+LowerAlphabet= []
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);

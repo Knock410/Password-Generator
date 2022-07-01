5f24cc3a121d220a4d54f291cc547fcf36a5855a
Knock <enoch10evelyn@gmail.com>
2022-06-30 20:41:32 -0700

    Declared Arrays

--- friendly-parakeet/Develop/script.js
+++ friendly-parakeet/Develop/script.js
@@ -8,7 +8,18 @@ var selected; // Choice that the user selects
 
 //Declaring arrays
 
-LowerAlphabet= []nn
+// Alphabet Lower case
+lowerAlphabet= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+// Alphabet Upper case
+upperAlphabet= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+// numeric 
+numbers=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+//special characters
+specialCharacters= ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"]
+
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
